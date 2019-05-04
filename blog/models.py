271daa91a5cd3f5ca7efe66b0047160bf98ed580
Bastian <bastian.matamala@ug.uchile.cl>
2019-05-04 16:38:42 -0400

    Evaluador

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Evaluador(models.Model):
+    name = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    mail = models.CharField(max_length=50)
+
+    def get_name(self):
+        return str(self.name + " " + self.lastname)
