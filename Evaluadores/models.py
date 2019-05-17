224032e5fdcff3d141edacd2cb4db55b6e0b3d02
Bastian <bastian.matamala@ug.uchile.cl>
2019-05-16 23:19:16 -0400

    Evaluadores: correo is now an EmailField

--- Evaluadores/models.py
+++ Evaluadores/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Evaluador(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    correo = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=50)
     creado = models.DateTimeField(auto_now=True)
 
     def get_name(self):
