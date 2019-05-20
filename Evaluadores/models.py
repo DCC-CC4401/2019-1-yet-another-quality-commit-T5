c9274cc1b6c80502c92cc12a524336ed85fd2720
bm <bastian.matamala@ug.uchile.cl>
2019-05-20 00:46:53 -0400

    Cada Evaluador ahora genera un Usuario de sistema

--- Evaluadores/models.py
+++ Evaluadores/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
 
 class Evaluador(models.Model):
     nombre = models.CharField(max_length=50)
@@ -20,4 +23,21 @@ class Evaluador(models.Model):
 
     def __str__(self):
         return str(self.nombre + " " + self.apellido)
-    
+
+    def save(self, *args, **kwargs):
+        # Agrega el modelo a la base de datos
+        super(Evaluador, self).save(*args, **kwargs)
+
+        # Genera un User
+        user = str(self.nombre).lower() + "." + str(self.apellido).lower()
+        password = User.objects.make_random_password()
+        user = User.objects.create_user(username=user,
+                                        email=self.correo,
+                                        password=password)
+        user.first_name = self.nombre
+        user.last_name = self.apellido
+
+        evaluadores = Group.objects.get(name='Evaluadores')
+        evaluadores.user_set.add(user)
+
+        user.save()
